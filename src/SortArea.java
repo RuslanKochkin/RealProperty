74c2ec2569d089cbe3631562651b1f43451215fe
Ruslan Kochkin <ruslan.vladimirovich.kochkin@gmail.com>
2023-05-21 01:48:12 +0200

    implement commit final commit new final commit

--- src/SortArea.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortArea implements Comparator <RealProperty> {
-    @Override
-    public int compare(RealProperty o1, RealProperty o2) {
-        return Double.compare(o1.getArea(), o2.getArea());
-        }
-    }
--- src/sortAddress.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class sortAddress implements Comparator<RealProperty> {
-
-    @Override
-    public int compare(RealProperty o1, RealProperty o2) {
-        return o1.getAddress().compareTo(o2.getAddress());
-    }
-}
